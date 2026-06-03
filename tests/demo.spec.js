8660b7f94aedaca0f8d38f19e428bc7de94fca95
ssangeethabharathi <pratheebacs123@gmail.com>
2026-06-03 02:30:01 -0700

    adding file

---
+++ tests/demo.spec.js
@@ -0,0 +1,32 @@
+import {test} from '@playwright/test';
+import { title } from 'node:process';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.facebook.com/');
+  await page.waitForTimeout(3000)
+  //URL()
+  const urlname=await page.url();
+  console.log(urlname)
+  //title()
+  console.log(await page.title());
+  await page.goBack();
+  await page.goForward();
+  await page.reload();
+  await page.waitForTimeout(3000);
+  await page.screenshot({path:'facebook.png'});
+  await page.waitForTimeout(2000)
+  
+  //dropdown
+  await page.goto("https://vinothqaacademy.com/drop-down/");
+  await page.selectOption
+
+
+
+
+
+
+  
+
+
+
+})
